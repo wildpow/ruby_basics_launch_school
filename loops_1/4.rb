e481fc8f2524ce34e1e171950b6bed7e5aa3b27c
Aaron Wilder <element1028@gmail.com>
2017-04-22 07:49:00 -0700

    loops

---
+++ loops_1/4.rb
@@ -0,0 +1,7 @@
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+  break if answer == 'yes'
+end
+
+# Modify the code below so the loop stops iterating when the user inputs 'yes'.
---
+++ loops_1/6.rb
@@ -0,0 +1,10 @@
+numbers = []
+
+while numbers.length < 5
+  rand_num = rand(-1..100)
+  puts rand_num
+  numbers << rand_num
+end
+
+# Using a while loop, print 5 random numbers between 0 and 99.
+# The code below shows an example of how to begin solving this exercise.
