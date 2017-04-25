54703f5e9be8bba92c0175b729f9e0a0eccab022
Aaron Wilder <element1028@gmail.com>
2017-04-24 23:55:35 -0700

    ok

--- user_input/5.rb
+++ user_input/5.rb
@@ -1,2 +1,4 @@
-puts "Enter number of lines"
-input - gets.chomp.to_i
+puts "How many output lines do you want? Enter a number >= 3:"
+input = gets.chomp.to_i
+for 
+puts "Launch School is the best!"
